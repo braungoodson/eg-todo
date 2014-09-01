fee32266b999913623d4002e29501a01b6a0703d
braun <braun@kermit.local>
2014-08-31 23:19:44 -0400

    rainbow sprinkles

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ angular
 	.factory('appFactory',[function(){return{};}])
 	.directive('appDirective',[function(){return{};}])
 ;
+
+angular
+	.module('egTodo',['ngCookies'])
+	.controller('egTodoController',['$scope','$cookies',function($scope,$cookies){
+    $scope.todo = JSON.parse($cookies.todo||JSON.stringify({todo:'something awesome',items:[]}));
+    $scope.add = function (item) {
+      $scope.todo.items.push(item);
+      $cookies.todo = JSON.stringify($scope.todo);
+    }
+    $scope.remove = function (index) {
+      $scope.todo.items.splice(index,1);
+      $cookies.todo = JSON.stringify($scope.todo);
+    }
+    $scope.ok = function ($index) {
+      if ($scope.todo.items[$index].ok) {
+        $scope.todo.items[$index].ok = false;
+      } else {
+        $scope.todo.items[$index].ok = true;
+      }
+      $cookies.todo = JSON.stringify($scope.todo);
+    }
+    $scope.warning = function ($index) {
+      if ($scope.todo.items[$index].warning) {
+        $scope.todo.items[$index].warning = false;
+      } else {
+        $scope.todo.items[$index].warning = true;
+      }
+      $cookies.todo = JSON.stringify($scope.todo);
+    }
+    $scope.fire = function ($index) {
+      if ($scope.todo.items[$index].fire) {
+        $scope.todo.items[$index].fire = false;
+      } else {
+        $scope.todo.items[$index].fire = true;
+      }
+      $cookies.todo = JSON.stringify($scope.todo);
+    }
+	}])
+;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express'),
+  cookies = require('cookie-parser'),
   server = express(),
   port = process.env.PORT || 30000,
   staticRoot = __dirname;
 
+server.use(cookies());
 server.use('/', express.static(staticRoot));
+server.get('/cookies',function(q,r){
+  return r.send(200);
+});
+server.post('/cookies',function(q,r){
+  return r.send(200);
+});
 server.listen(port);
 console.log('http://localhost:'+port);
